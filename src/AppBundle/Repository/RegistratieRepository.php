78d270234f0ea37c02ec574e5b077652995dcacc
MikeyBtheG <olybol3@live.nl>
2018-06-05 15:14:31 +0200

    zuig een dikke pik kyl v2

---
+++ src/AppBundle/Repository/RegistratieRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mike
+ * Date: 5-6-2018
+ * Time: 14:13
+ */
+
+namespace AppBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class RegistratieRepository extends EntityRepository
+{
+
+}
