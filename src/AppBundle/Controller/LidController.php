fc528165f739596e42d88b6d5114fd20a08e780b
MikeyBtheG <olybol3@live.nl>
2018-06-05 12:36:23 +0200

    lessen bekijken

--- src/AppBundle/Controller/LidController.php
+++ src/AppBundle/Controller/LidController.php
@@ -9,6 +9,7 @@
 namespace AppBundle\Controller;
 
 
+use AppBundle\Entity\Les;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,10 +19,20 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class LidController extends Controller
 {
     /**
-     * @Route("/lid", name = "lid")
-     */
+ * @Route("/lid", name = "lid")
+ */
     public function showAction()
     {
         return $this->render('/lid/start.html.twig');
+    }
+
+    /**
+     * @Route("/lid/lessen", name = "lessen")
+     */
+    public function lessenAction()
+    {
+        $repository=$this->getDoctrine()->getRepository(Les::class);
+        $lessen=$repository->findAll();
+        return $this->render('lid/lessen.html.twig',array('boodschap'=>'Welkom','lessen'=>$lessen));
     }
 }
